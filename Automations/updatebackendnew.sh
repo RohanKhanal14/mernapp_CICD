1c4e4676b5bee2d340c7715d758af22d29959514
Rohan Khanal <125800654+RohanKhanal14@users.noreply.github.com>
2025-08-26 22:26:21 +0545

    Update backend script to use Minikube IP

--- Automations/updatebackendnew.sh
+++ Automations/updatebackendnew.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-# Set the Instance ID and path to the .env file
-INSTANCE_ID="i-030da7d31a1dbbffc"
-
-# Retrieve the public IP address of the specified EC2 instance
-ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+# Get Minikube IP 
+minikube_ip=$(minikube ip)
 
 # Path to the .env file
-file_to_find="../backend/.env.docker"
+file_to_find="../frontend/.env.docker"
 
-# Check the current FRONTEND_URL in the .env file
-current_url=$(sed -n "4p" $file_to_find)
+# Check the current VITE_API_PATH in the .env file
+current_url=$(cat $file_to_find)
 
 # Update the .env file if the IP address has changed
-if [[ "$current_url" != "FRONTEND_URL=\"http://${ipv4_address}:5173\"" ]]; then
+if [[ "$current_url" != "VITE_API_PATH=\"http://${minikube_ip}:31100\"" ]]; then
     if [ -f $file_to_find ]; then
-        sed -i -e "s|FRONTEND_URL.*|FRONTEND_URL=\"http://${ipv4_address}:5173\"|g" $file_to_find
+        sed -i -e "s|VITE_API_PATH.*|VITE_API_PATH=\"http://${minikube_ip}:31100\"|g" $file_to_find
     else
         echo "ERROR: File not found."
     fi
